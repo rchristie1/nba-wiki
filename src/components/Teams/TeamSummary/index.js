8f6d8ab341a016ef1d76add1beda21db6766a762
rchristie1 <m.christie.ryan@gmail.com>
2019-07-29 22:54:20 -0400

    feature: update tabs on team page and centralize styling for tabs

--- src/components/Teams/TeamSummary/index.js
+++ src/components/Teams/TeamSummary/index.js
@@ -8,15 +8,13 @@ import { TeamLogo } from '../../../config';
 import TeamContext from '../../../context/TeamContext';
 
 const TeamSummary = () => {
+  const { teamDetails, seasonRankings, teamRoster, teamGameLog } = useContext(TeamContext);
 
-  const {teamDetails, seasonRankings, teamRoster, teamGameLog} = useContext(TeamContext);
-  
   const td = teamDetails;
   const sr = seasonRankings;
-  const [tab, setTab] = useState(false);    
+  const [tab, setTab] = useState(false);
 
   return (
-    
     <div className={styles.teamContainer}>
       <div className={styles.teamSummary}>
         <div className={styles.left}>
@@ -26,33 +24,36 @@ const TeamSummary = () => {
 
           <div className={styles.teamName}>
             <h1>{`${td[2]} ${td[3]}`}</h1>
+            <div className={styles.keyStats}>
+              <h2 className={td[5] === 'East' ? styles.east : styles.west}>{`${td[5]}ern Conference`}</h2>
+              <h3>{`W:${td[8]} L:${td[9]}`}</h3>
+            </div>
           </div>
         </div>
+
+        <div className={styles.divider} />
 
         <div className={styles.right}>
           <div className={styles.teamDetails}>
             <ul>
-              <li>{`Conference: ${td[5]}ern`}</li>
+              <li>{`Win Rate: ${td[10] * 100}%`}</li>
               <li>{`Division: ${td[6]}`}</li>
-              <li>{`Season Record: W:${td[8]} L:${td[9]}`}</li>
-              <li>{`Win %${td[10]}`}</li>
               <li>{`Conference Ranking: ${td[11]}`}</li>
               <li>{`Division Ranking: ${td[12]}`}</li>
+              <li>{`Points Rank: ${sr[3]}`}</li>
+              <li>{`Points Against Ranking: ${sr[9]}`}</li>
             </ul>
           </div>
 
-          <div className={styles.divider} />
-
           <div className={styles.seasonRankings}>
             <ul>
-              <li>{`Points Rank: ${sr[3]}`}</li>
-              <li>{`PPG: ${sr[4]}`}</li>
-              <li>{`Rebounds Ran: ${sr[5]}`}</li>
-              <li>{`RPG: ${sr[6]}`}</li>
-              <li>{`Assists Rank: ${sr[7]}`}</li>
               <li>{`APG: ${sr[8]}`}</li>
-              <li>{`Opponent Points Ranking: ${sr[9]}`}</li>
+              <li>{`PPG: ${sr[4]}`}</li>
+              <li>{`RPG: ${sr[6]}`}</li>
               <li>{`OPPG: ${sr[10]}`}</li>
+              <li>{`Assists Rank: ${sr[7]}`}</li>
+              <li>{`Rebounds Rank: ${sr[5]}`}</li>
+              
             </ul>
           </div>
         </div>
@@ -60,18 +61,14 @@ const TeamSummary = () => {
 
       <div>
         <div className={styles.tabs}>
-          <div className={!tab ? styles.active : undefined} onClick={() => setTab(false)}>
+          <div className={!tab ? styles.tabSelected : undefined} onClick={() => setTab(false)}>
             Roster/Coaching Staff
           </div>
-          <div className={tab ? styles.active : undefined} onClick={() => setTab(true)}>
+          <div className={tab ? styles.tabSelected : undefined} onClick={() => setTab(true)}>
             Game Log
           </div>
         </div>
-        {tab ? (
-          <TeamGameLog data={teamGameLog} />
-        ) : (
-          <TeamRoster data={teamRoster} />
-        )}
+        {tab ? <TeamGameLog data={teamGameLog} /> : <TeamRoster data={teamRoster} />}
       </div>
     </div>
   );
